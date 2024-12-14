48178a61eb5c06c59126e1c20e3aa5e5cb1dd89e
Kartik Patel <karthikcpatel@gmail.com>
2024-12-15 01:27:07 +0530

    8th Dec, 2024 - Added new files and modified the logic of existing files.

--- python programs/Count of repeated words in list.py
+++ python programs/Count of repeated words in list.py
@@ -1,6 +1,14 @@
-list = ["a", "b", "a", "c", "c", "a", "c"]
+list = ["a", "b", "a", "c", "c", "a", "c", "k", "z"]
 result = {}
 for i in list:
-    result[i] = list.count(i)
+    if list.count(i) > 1:
+        result[i] = list.count(i)
+print(result)
+
+list = [10,10,20,30,40,50]
+result = {}
+for i in list:
+    if list.count(i) > 1:
+        result[i] = list.count(i)
 print(result)
 
